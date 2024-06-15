5fe0358b5334ece549fbb6334315e17589e8cab1
Naoto MATSUMOTO <mnaoto48@gmail.com>
2024-06-15 22:39:01 +0900

    Fix: lambda_function.py: レスポンスに含まれる情報に初期値を設定するほか、環境変数を参照するように修正

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -1,5 +1,6 @@
-from jinja2 import FileSystemLoader, Environment
+from jinja2 import Template, FileSystemLoader, Environment
 import boto3
+import os
 
 # S3呼び出し
 s3 = boto3.client('s3')
@@ -7,16 +8,31 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     env = Environment(loader=FileSystemLoader('.'))
+    
+    # レスポンスに含む情報を初期化
+    name = 'Guest'
+    is_login = False
+    
     # S3からテンプレート読み込み
     template_str = s3.get_object(Bucket='ac-book-ma-footprints', Key='index.html')['Body'].read().decode('utf-8')
 
     # jinja2で扱えるテンプレートに変換する
     template = Template(template_str)
+    
+    # 環境変数から必要な情報を取得
+    # db_host =
+    # db_port = 
+    # db_username =
+    # db_password =
+    # db_database = 
+    if os.environ['DEV_NAME'] != False:
+      name = os.environ['DEV_NAME']
+    is_login = os.environ['DEV_IS_LOGIN']
 
     # レンダリング内容
     data = {
-        "name": "MATCHA",
-        "is_login": True
+        "name": name,
+        "is_login": is_login
     }
 
     # templateにdataを埋め込む
@@ -29,3 +45,4 @@ def lambda_handler(event, context):
       },
       'body': rendered
     }
+
